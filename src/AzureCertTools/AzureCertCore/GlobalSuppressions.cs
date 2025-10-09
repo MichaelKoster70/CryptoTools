bc30534e28acbf9e0fa3393c49c3a54ad22620e5
Michael Koster <michael.koster@agi-sda.com>
2025-10-09 19:36:19 +0200

    fix broken build

--- src/AzureCertTools/AzureCertCore/GlobalSuppressions.cs
+++ src/AzureCertTools/AzureCertCore/GlobalSuppressions.cs
@@ -1,7 +1,12 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+﻿// ----------------------------------------------------------------------------
+// <copyright company="Michael Koster">
+//   Copyright (c) Michael Koster. All rights reserved.
+//   Licensed under the MIT License.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+// This file is used by Code Analysis to maintain SuppressMessage attributes that are applied to this project.
+// Project-level suppressions either have no target or are given a specific target and scoped to a namespace, type, member, etc.
 
 using System.Diagnostics.CodeAnalysis;
 
