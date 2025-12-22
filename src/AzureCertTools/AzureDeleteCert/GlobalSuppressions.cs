4d60f783fef2b36e7069afc10c46394f9de18220
Michael Koster <michael.koster@agi-sda.com>
2025-12-22 14:39:24 +0100

    fix review suggestions

--- src/AzureCertTools/AzureDeleteCert/GlobalSuppressions.cs
+++ src/AzureCertTools/AzureDeleteCert/GlobalSuppressions.cs
@@ -10,7 +10,4 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Instantiated via CommandLineParser reflection.", Scope = "type", Target = "~T:CertTools.AzureCreateSigningCert.Options")]
-[assembly: SuppressMessage("CodeSmell", "S1075:Refactor your code not to use hardcoded absolute paths or URIs", Justification = "Hardcoded URI is required for authentication redirect.", Scope = "member", Target = "~M:CertTools.AzureCreateSigningCert.Program.Main(System.String[])")]
-[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "By design Windows only", Scope = "member", Target = "~M:CertTools.AzureCreateSigningCert.CertificateWorker.LocalCreateSigningCertificateAsync(System.String,System.String,System.String,System.String,System.Uri,Azure.Core.TokenCredential,System.Int32)~System.Threading.Tasks.Task")]
 [assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Required for graceful error handling", Scope = "member", Target = "~M:CertTools.AzureDeleteCert.Program.Main(System.String[])~System.Int32")]
