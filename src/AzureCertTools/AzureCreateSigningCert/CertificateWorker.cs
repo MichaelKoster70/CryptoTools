735f3461f2c15f9a711c00869846008257a38370
Sergey Efremov <sergey.efremov@agi-sda.com>
2026-06-05 14:26:21 +0100

    Explicitly mark private keys as non-exportable

--- src/AzureCertTools/AzureCreateSigningCert/CertificateWorker.cs
+++ src/AzureCertTools/AzureCreateSigningCert/CertificateWorker.cs
@@ -95,6 +95,7 @@ internal static class CertificateWorker
          KeyType = CertificateKeyType.Rsa,
          KeySize = CertificateWorkerCore.RsaKeySize,
          ReuseKey = true,
+         Exportable = false,
          ValidityInMonths = expireMonth,
       };
 
